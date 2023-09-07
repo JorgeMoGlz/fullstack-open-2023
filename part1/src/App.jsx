5765dc410e8825aee290ff708bab48b64d9b182d
JorgeMoGlz <jorgemoglz95@gmail.com>
2023-09-07 17:21:09 -0600

    React-JSX

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,18 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-export const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
+const Hello = () => (
+  <div>
+    <p>Hello world</p>
+  </div>
+)
 
+export const App = () => {
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a+b}
-      </p>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   )
 }
