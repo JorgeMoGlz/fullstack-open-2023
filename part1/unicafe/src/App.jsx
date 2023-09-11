97cf10f98aecd345059b346d29fd720fcc35848a
JorgeMoGlz <jorgemoglz95@gmail.com>
2023-09-11 14:22:49 -0600

    1.7: unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,19 +12,31 @@ export const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const [score, setScore] = useState(0)
+
+  const [total, setTotal] = useState(0)
+
   const handleGoodClick = () => {
     const updatedGood = good+1
     setGood(updatedGood)
+
+    setTotal(updatedGood+neutral+bad)
+    setScore(score+1)
   }
   
   const handleNeutralClick = () => {
     const updatedNeutral = neutral+1
     setNeutral(updatedNeutral)
+
+    setTotal(good+updatedNeutral+bad)
   }
   
   const handleBadClick = () => {
     const updatedBad = bad+1
     setBad(updatedBad)
+
+    setTotal(good+neutral+updatedBad)
+    setScore(score-1)
   }
 
   return (
@@ -35,9 +47,14 @@ export const App = () => {
       <Button handleClick={handleBadClick} text='bad' />
 
       <h3>statistics</h3>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {total}</p>
+      
+
+      <p>average: {(!score/total) ? '0' : (score/total).toFixed(3)}</p>
+      <p>positive: {(!good/total*100) ? '0' : (good/total*100).toFixed(3)} %</p>
 
     </div>
   )
